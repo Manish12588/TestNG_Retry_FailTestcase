e1c5e130230b829812db7499a765072d2cc0a438
“Manish12588” <manish1561988@gmail.com>
2019-11-24 18:45:06 +0530

    Retry Fail Testcase using RetryListner class

--- TestNG_Retry_FailTestcase/src/main/java/pages/SecureAreaPage.java
+++ TestNG_Retry_FailTestcase/src/main/java/pages/SecureAreaPage.java
@@ -1,4 +1,4 @@
-	package pages;
+package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,15 +7,21 @@ public class SecureAreaPage {
 	private WebDriver driver;
 
 	private By statusAlert = By.cssSelector("#flash");
+	private By logoutButton= By.xpath("//i[text()=' Logout']/..");
 	
+
 	public SecureAreaPage(WebDriver driver) {
 		this.driver= driver;
 	}
-	
+
 	public String getAlertText() {
 		return driver.findElement(statusAlert).getText();
-		 
+
+	}
+
+	public void clickLogoutButton() {
+		driver.findElement(logoutButton).click();
+
 	}
 	
-
 }
--- TestNG_Retry_FailTestcase/src/test/java/login/LoginTests.java
+++ TestNG_Retry_FailTestcase/src/test/java/login/LoginTests.java
@@ -2,6 +2,7 @@ package login;
 
 import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import base.BaseTests;
@@ -10,19 +11,34 @@ import pages.SecureAreaPage;
 
 public class LoginTests extends BaseTests{
 
+	LoginPage loginPage;
+	@BeforeClass
+	public void LoginTests1() {
+		// TODO Auto-generated constructor stub\
+		loginPage=homePage.clickFormAuthentication();
+	}
 	
-	@Test
+	@Test (priority = 1)
 	public void testSuccessfulLogin() {
-		
-		LoginPage loginPage=homePage.clickFormAuthentication();
 		loginPage.setUsername("tomsmith");
 		loginPage.setPassword("SuperSecretPassword!");
 		SecureAreaPage secureAreaPage=loginPage.clickLoginButton();
 		assertTrue(secureAreaPage.getAlertText()
 				.contains("You logged into a secure area!"),
 				"Alert text is incorrect");
+		secureAreaPage.clickLogoutButton();
+			
+	}
+		
+	@Test (priority = 2)
+	public void testUnsuccessfulLogin() {
+		loginPage.setUsername("tomsmith");
+		loginPage.setPassword("SuperSecretPassword");
+		SecureAreaPage secureAreaPage=loginPage.clickLoginButton();
+		assertTrue(secureAreaPage.getAlertText()
+				.contains("You logged into a secure area!"),
+				"Alert text is incorrect");
 		
 	}
-	
 	
 }
